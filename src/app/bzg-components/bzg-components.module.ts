2622e31cfb661df56e3c55a8b8828d8aacaa9cd2
irtaimal <ir.taimal10@uniandes.edu.co>
2017-12-12 00:25:43 -0500

    disable components  to test bzg-module

--- src/app/bzg-components/bzg-components.module.ts
+++ src/app/bzg-components/bzg-components.module.ts
@@ -1,12 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {BzgModalComponent} from './bzg-modal/bzg-modal.component';
+import {FormsModule} from "@angular/forms";
+/*import {BzgModalComponent} from './bzg-modal/bzg-modal.component';
 import {BzgTabSetComponent} from './bzg-tab-set/bzg-tab-set.component';
 import {BzgTabItemComponent} from './bzg-tab-item/bzg-tab-item.component';
 import {BzgComboComponent} from './bzg-combo/bzg-combo.component';
-import {FormsModule} from "@angular/forms";
 import {BzgStepSetComponent} from './bzg-step-set/bzg-step-set.component';
-import {BzgStepItemComponent} from './bzg-step-item/bzg-step-item.component';
+import {BzgStepItemComponent} from './bzg-step-item/bzg-step-item.component';*/
 import {BzgTooltipComponent} from "./bzg-tooltip/bzg-tooltip.component";
 import {BzgOverlayManager} from "./bzg-tooltip/bzg-overlay-manager";
 
@@ -16,21 +16,21 @@ import {BzgOverlayManager} from "./bzg-tooltip/bzg-overlay-manager";
         FormsModule
     ],
     exports: [
-        BzgModalComponent,
+       /* BzgModalComponent,
         BzgTabSetComponent,
         BzgTabItemComponent,
         BzgComboComponent,
         BzgStepSetComponent,
-        BzgStepItemComponent,
+        BzgStepItemComponent,*/
         BzgTooltipComponent
     ],
     declarations: [
-        BzgModalComponent,
+       /* BzgModalComponent,
         BzgTabSetComponent,
         BzgTabItemComponent,
         BzgComboComponent,
         BzgStepSetComponent,
-        BzgStepItemComponent,
+        BzgStepItemComponent,*/
         BzgTooltipComponent
     ],
     providers: [BzgOverlayManager],
