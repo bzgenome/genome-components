395b34005b3640245abfe6f55d6eca3dadb89838
ivantaimal <ivan.taimal@bizagi.com>
2018-02-18 01:02:44 -0500

    Include component bzg-user

--- src/app/bzg-components/bzg-button/bzg-button.component.ts
+++ src/app/bzg-components/bzg-button/bzg-button.component.ts
@@ -1,43 +1,39 @@
 import {Component, EventEmitter, Inject, Input, OnInit, Output} from '@angular/core';
 
 @Component({
-    selector: 'bzg-button',
-    templateUrl: './bzg-button.component.html',
-    styleUrls: ['./bzg-button.component.scss']
+  selector: 'bzg-button',
+  templateUrl: './bzg-button.component.html',
+  styleUrls: ['./bzg-button.component.scss']
 })
 export class BzgButtonComponent implements OnInit {
 
-    @Output() click: EventEmitter<any>;
-    @Input() iconClass?: string;
-    @Input() text?: string;
+  @Output() click: EventEmitter<any>;
+  @Input() srcIcon?: string;
+  @Input() text?: string;
 
-    constructor(@Inject('genomeConfig') private config: any) {
-        this.click = new EventEmitter<any>();
-    }
+  constructor(@Inject('genomeConfig') private config: any) {
+    this.click = new EventEmitter<any>();
+  }
 
-    ngOnInit() {
+  ngOnInit() {
 
-    }
+  }
 
-    public triggerClick() {
-        this.click.emit();
-    }
+  public triggerClick() {
+    this.click.emit();
+  }
 
-    public getIconClass() {
-        return this.iconClass ? this.iconClass : false;
-    }
+  public getText() {
+    return this.text ? this.text : false;
+  }
 
-    public getText() {
-        return this.text ? this.text : false;
-    }
-
-    public getStyles() {
-        return {
-            // CSS property names
-            'border': '1px solid ' + this.config["color-button-primary"],
-            'background-color': this.config["color-button-primary"],
-            'font-weight': 'normal',  // normal
-        };
-    }
+  public getStyles() {
+    return {
+      // CSS property names
+      'border': '1px solid ' + this.config["color-button-primary"],
+      'background-color': this.config["color-button-primary"],
+      'font-weight': 'normal',  // normal
+    };
+  }
 
 }
